161ebd886770b1e4c1e7eb1060477c3ec58aac68
nankehang <nankehang2016@gmail.com>
2025-10-03 18:39:25 +0700

    fix again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
-    optimizeCss: false, // ปิดการใช้ lightningcss
+    optimizeCss: false, // แก้ปัญหา lightningcss บน Railway
   },
 };
 
